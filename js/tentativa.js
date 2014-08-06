9dae0d3ca95c0d41d96fceaba8e3c174e25d09c4
joao vidal <joao.vidal@exla.com.br>
2014-08-06 17:29:24 -0300

    formulario

--- js/tentativa.js
+++ js/tentativa.js
@@ -34,4 +34,36 @@ function ajax(a, b){
 
  }
 
-$(document).ready(ajax(1, 10))
+$(document).ready(ajax(1, 100))
+
+$(function(){
+ 
+    $("#button").click(function(){
+ 
+        var a1 = $("input[name=field1]").val();
+        var b2 = $("textarea[name=field4]").val();
+        var c3 = parseFloat($("input[name=field7]").val());
+        var d4 = parseFloat($("input[name=field10]").val());
+        //var cerveja = {
+             //"nome": "skol",
+             //"descr": "lol"
+        //}
+        $.ajax({
+
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
+                xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5"); 
+            },
+             
+            type: "POST",
+            data: JSON.stringify({"nome":a1,
+                                "descr":b2,
+                                "teor":c3,
+                                "preco":d4}),
+            url: 'https://api.parse.com/1/classes/Cervejas',
+            dataType: "json",
+            contentType: "application/json; charset=UTF-8"
+
+        })            
+    });
+});
