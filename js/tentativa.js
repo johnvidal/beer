33c19a05ab69122c4ceeefa793cee2580c14d458
joao vidal <joao.vidal@exla.com.br>
2014-08-13 09:24:57 -0300

    site lvl 2 update 2.0

--- js/tentativa.js
+++ js/tentativa.js
@@ -1,48 +1,26 @@
-/* funçãos para carregar o parse */
-function carregar(bbbb){
+function jQuerySubmit(type, url, retorno) {
     $.ajax({
         beforeSend: function(xhrObj){
             xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
             xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5");                
         },
-        type:"DELETE",
-        url:'https://api.parse.com/1/classes/Cervejas/'+ bbbb,
-        processData:false,
-        dataType:"json",
-    }).then(function(){
-        document.getElementById("section1").innerHTML = ""
-        loadParse(1, 100)
-    })
+        type: type,
+        url: url,
+        processData: false,
+        dataType: "json",
+        success: function(data){ retorno(data)},
+        error: function(){ alert("Erro ao carregar PARSE")}
+    });
 }
 
-function modal() {
-    $( "#modalCreate" ).dialog({
-      modal: true,
-      buttons: {
-        Ok: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-  };
+/* funçãos AJAX*/
 
 function loadParse(a, b){
 
-        $.ajax({
-                        beforeSend: function(xhrObj){
-                                xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
-                                xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5");                
-                        },
-                        type:"GET",
-                        url:'https://api.parse.com/1/classes/Cervejas/?where={"teor":{"$gte":'+a+',"$lte":'+ b +'}}',
-                        processData:false,
-                        dataType:"json",
-                }).then(function(data) {
+        jQuerySubmit("GET",'https://api.parse.com/1/classes/Cervejas/?where={"teor":{"$gte":'+a+',"$lte":'+ b +'}}', function(data) {
                             
 
                    for(var i = 0; i < data.results.length; i++){
-
-                        var carregandoObjeto = data.results[i].objectId
 
                         jQuery("#section1").append('<div class="blocos">' +
                             '<div class="col3">' +
@@ -50,16 +28,141 @@ function loadParse(a, b){
                             '<p>'+data.results[i].descr+'</p>' +
                             '<h5>'+data.results[i].teor+'</h5>' +
                             '<h5>'+data.results[i].preco+'</h5>' +
-                            '<button class="buttonDel" onclick="carregar(\''+ data.results[i].objectId +'\')" value="delete">Delete</button>'+
-                            '<button class="buttonEdit" id="buttonEdit" onclick="modal()" value="Editar">Editar</button>'+
+                            '<h6>'+data.results[i].objectId+'</h6>' + 
+                            '<button class="buttonDel" onclick="deletar(\''+ data.results[i].objectId +'\')" value="delete">Delete</button>'+
+                            '<button class="buttonEdit" id="buttonEdit" onclick="getOne(\''+ data.results[i].objectId +'\')" value="Editar">Editar</button>'+
                             '</div>' +
                             '<ul id="ulCall"></ul>' +
                             '</div>');
-                    }    
+                        
+                };    
         });
 };
 
-/* filtro de Teor Alcoólico */
+function getOne(getID2){
+    jQuerySubmit("GET",'https://api.parse.com/1/classes/Cervejas/'+getID2, function(data){
+        jQuery("#modalEdit").append('<div id="editName" class="field f_100">'+
+                    '<input type="text" name="editName" id="editName" required="required" value="'+data.nome+'">'+ '</div>'+
+                    '<div id="editDescr" class="field f_100">'+
+                    '<textarea rows="5" cols="20" name="editDescr" id="editDescr" required="required">'+data.descr+'</textarea>'+'</div>'+
+                    '<div id="editTeor" class="field f_100">'+
+                    '<input type="number" name="editTeor" id="editTeor" required="required" value="'+data.teor+'">'+'</div>'+
+                    '<div id="editPreco" class="field f_100">'+
+                     '<input type="number" name="editPreco" id="editPreco" required="required" value="'+data.preco+'">'+'</div>'
+                ); 
+    $( "#modalEdit" ).dialog({
+          modal: true,
+          buttons: {
+                Ok: function() {
+
+                    var cervejasNome = $("input[name=editName]").val();
+                    var cervejasDescr = $("textarea[name=editDescr]").val();
+                    var cervejasTeor = parseFloat($("input[name=editTeor]").val());
+                    var cervejasPreco = parseFloat($("input[name=editPreco]").val());
+                    
+                  $.ajax({
+                    beforeSend: function(xhrObj){
+                        xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
+                        xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5");                
+                    },
+                    type:"PUT",
+                    url:'https://api.parse.com/1/classes/Cervejas/'+ getID2,
+                    processData:false,
+                    dataType:"json",
+                    data: JSON.stringify({"nome":cervejasNome,
+                                            "descr":cervejasDescr,
+                                            "teor":cervejasTeor,
+                                            "preco":cervejasPreco}),
+                    contentType: "application/json; charset=UTF-8"
+                    })
+         
+                    document.getElementById("section1").innerHTML = ""
+                    document.getElementById("modalEdit").innerHTML = ""
+                    loadParse(1, 100)
+    
+                    $('#modalEdit').on('hidden', function () {
+                        $('input').val('');
+                    });
+
+                    $( this ).dialog( "close" );
+                },
+                Close: function(){
+                    document.getElementById("modalEdit").innerHTML = ""
+    
+                    $('#modalEdit').on('hidden', function () {
+                        $('input').val('');
+                    });
+
+                    $( this ).dialog( "close" );
+
+                }
+            }
+        })
+    })
+} 
+
+
+function deletar(idObject){
+    jQuerySubmit("DELETE",'https://api.parse.com/1/classes/Cervejas/'+ idObject, function(data){
+        document.getElementById("section1").innerHTML = ""
+        loadParse(1, 100)
+    })
+};
+
+/* validador do cadastro */
+
+function cadastro(){
+ 
+    $( "#modalCadastro" ).dialog({
+          modal: true,
+          buttons: {
+                    Ok: function(){
+                        var cervejasNome = $("input[name=enterName]").val();
+                        var cervejasDescr = $("textarea[name=enterDescr]").val();
+                        var cervejasTeor = parseFloat($("input[name=enterTeor]").val());
+                        var cervejasPreco = parseFloat($("input[name=enterPreco]").val());
+                       
+                        $.ajax({
+
+                            beforeSend: function(xhrObj){
+                                xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
+                                xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5"); 
+                            },
+                             
+                            type: "POST",
+                            data: JSON.stringify({"nome":cervejasNome,
+                                                    "descr":cervejasDescr,
+                                                    "teor":cervejasTeor,
+                                                    "preco":cervejasPreco}),
+                            url: 'https://api.parse.com/1/classes/Cervejas',
+                            dataType: "json",
+                            contentType: "application/json; charset=UTF-8"
+
+                            })
+                        document.getElementById("section1").innerHTML = ""
+                        document.getElementById("modalCadastro").innerHTML = ""
+                        loadParse(1, 100)
+        
+                        $('#modalEdit').on('hidden', function () {
+                            $('input').val('');
+                        });
+
+                        $( this ).dialog( "close" );
+                    },
+                    Close: function(){
+                    document.getElementById("modalEdit").innerHTML = ""
+    
+                    $('#modalEdit').on('hidden', function () {
+                        $('input').val('');
+                    });
+
+                    $( this ).dialog( "close" );
+                    }     
+        }          
+    });
+};
+
+/* filtro de Teor Alcoólico */ 
 
 function getValue(){
     var a = document.getElementById("min").value
@@ -73,30 +176,6 @@ $(document).ready(loadParse(1, 100))
 /* prompt de cadastro */
 
 
-/* validador do cadastro*/
 
-$(function(){
- 
-    $("#button").click(function(){
- 
-        var cervejasNome = {"nome":$("input[name=enterName]").val()};
-        var cervejasDescr = {"descr":$("textarea[name=enterDescr]").val()};
-        var cervejasTeor = {"teor":parseFloat($("input[name=enterTeor]").val())};
-        var cervejasPreco = {"preco":parseFloat($("input[name=enterPreco]").val())};
-       
-        $.ajax({
 
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
-                xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5"); 
-            },
-             
-            type: "POST",
-            data: JSON.stringify(cervejasNome, cervejasDescr, cervejasTeor, cervejasPreco),
-            url: 'https://api.parse.com/1/classes/Cervejas',
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8"
 
-        })            
-    });
-});
