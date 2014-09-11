5d2481b90c3987f91f8742b01d3e0337f82dffc4
joao vidal <joao.vidal@exla.com.br>
2014-09-11 14:51:38 -0300

    Angular Site - 1.2

--- js/bebidasController.js
+++ js/bebidasController.js
@@ -1,5 +1,22 @@
 var bebidasApp = angular.module('bebidasApp', []);
 
+
+function atualizar(scope, funcao) {
+
+    if (scope.$$phase || scope.$root.$$phase) {
+		if (typeof funcao !== 'function') {
+			return;
+		}    	
+		funcao();
+    }  else {
+		if (typeof funcao !== 'function') {
+	    	scope.$apply();
+    	} else {
+    		scope.$apply(funcao);
+    	}
+	}
+}
+
 function jQuerySubmit(type, url, retorno) {
     $.ajax({
         beforeSend: function(xhrObj){
@@ -17,15 +34,41 @@ function jQuerySubmit(type, url, retorno) {
 
 bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
 	$scope.bebidas = [];
+
+	$scope.paises = ['Brasil', 'Bélgica', 'Estados Unidos', 'Holanda', 'Irlanda', 'México', 'Dinamarca'];
 	$scope.pagina = 0;
+
+	$scope.reload = function (numeroPag) {
+		$scope.pagina = numeroPag;
+		$scope.bebidas = $scope.todasBebidas;
+	}
+
 	$scope.definirPagina = function (numeroPagina) {
 		$scope.pagina = numeroPagina;
+	};
+
+	$scope.definirPaises = function (nomePais) {
+		var bebidas = $scope.todasBebidas,
+			numeroBebidas = bebidas.length,
+			indiceBebida,
+			bebida,
+			bebidasPais = [];
 		
+		for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
+			bebida = bebidas[indiceBebida];
+			if (bebida.origem === nomePais) {
+				bebidasPais.push(bebida);
+			}
+		}
+
+		$scope.bebidas = bebidasPais;
+		atualizar($scope);
 	};
 
 	jQuerySubmit("GET",'https://api.parse.com/1/classes/Cervejas', function (data) {
-		$scope.bebidas = data.results;
-		$scope.$apply();
+		var bebidas = data.results;
+		$scope.todasBebidas = bebidas;
+		$scope.bebidas = bebidas;
+		atualizar($scope);
 	})
-
 }]);
