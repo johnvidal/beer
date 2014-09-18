904179c0a9a86643f038f94b1565f7e2719e0f6d
joao vidal <joao.vidal@exla.com.br>
2014-09-18 17:33:55 -0300

    angular site - 2.0

--- js/bebidasController.js
+++ js/bebidasController.js
@@ -33,16 +33,30 @@ function jQuerySubmit(type, url, retorno) {
 }
 
 bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
+	var bebidaInicial = {
+		nome: '',
+		descr: '',
+		preco: '',
+		teor: '',
+		origem: '',
+		estilo: '',
+		bandeira: ''
+	};
 	$scope.bebidas = [];
 
-	$scope.paises = ['Brasil', 'Bélgica', 'Estados Unidos', 'Holanda', 'Irlanda', 'México', 'Dinamarca'];
+	$scope.paises = ['Brasil', 'Bélgica', 'Estados Unidos', 'Holanda', 'Irlanda', 'México', 'Dinamarca', 'Alemanha'];
+	$scope.estilos = ['Large', 'Pilsen'];
 	$scope.pagina = 0;
-	$scope.linhaExcluir = null;
+	$scope.bebidaExcluir = null;
 	$scope.detalheEscolhido = null;
+	$scope.bebida = bebidaInicial;
 
 	$scope.reload = function (numeroPag) {
 		$scope.pagina = numeroPag;
 		$scope.bebidas = $scope.todasBebidas;
+		$scope.detalheEscolhido = null;
+		$scope.bebidaEditada = null;
+		$scope.bebida = bebidaInicial;
 	}
 
 	$scope.definirPagina = function (numeroPagina) {
@@ -62,8 +76,7 @@ bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
 				bebidasPais.push(bebida);
 			}
 		}
-
-		$scope.bebidas = bebidasPais;
+		$scope.bebidas = bebidasPais;face1
 		atualizar($scope);
 	};
 
@@ -75,8 +88,26 @@ bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
 		}
 	};
 
-	$scope.excluir = function (objectId) {
-		$scope.linhaExcluir = objectId;
+	$scope.editarBebida = function (objectId) {
+		var bebidas = $scope.bebidas,
+			numeroBebidas = bebidas.length,
+			indiceBebida,
+			bebida;
+
+		$scope.bebidaEditada = objectId;
+		for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
+			bebida = bebidas[indiceBebida];
+			if (bebida) {
+				if (bebida.objectId === objectId) {
+					$scope.bebida = bebida;
+					break;
+				}
+			}
+		}
+	};
+
+	$scope.excluir = function () {
+		$scope.bebidaExcluir = $scope.bebidaEditada;
 	};
 
 	$scope.confirmarExcluir = function () {
@@ -85,24 +116,25 @@ bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
 			indiceBebida,
 			bebida;
 
-		jQuerySubmit("DELETE",'https://api.parse.com/1/classes/Cervejas/'+ $scope.linhaExcluir, function(data) {
+		jQuerySubmit("DELETE",'https://api.parse.com/1/classes/Cervejas/'+ $scope.bebidaExcluir, function(data) {
 			for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
 				bebida = bebidas[indiceBebida];
 				if (bebida) {
-					if (bebida.objectId === $scope.linhaExcluir) {
+					if (bebida.objectId === $scope.bebidaExcluir) {
 						bebidas.splice(indiceBebida, 1);
 						break;
 					}
 				}
 			}
-			$scope.linhaExcluir = null;
+			$scope.bebidaExcluir = null;
 			$scope.bebidas = bebidas;
+			$scope.bebida = bebidaInicial;
 			atualizar($scope);
 		});
 	};
 
 	$scope.cancelarExcluir = function () {
-		$scope.linhaExcluir = null;
+		$scope.bebidaExcluir = null;
 		atualizar($scope);
 	};
 
@@ -127,3 +159,12 @@ bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
 		    });
 	}*/
 }]);
+
+function maxLength(el) {    
+    if (!('maxLength' in el)) {
+        var max = el.attributes.maxLength.value;
+        el.onkeypress = function () {
+            if (this.value.length >= max) return false;
+        };
+    }
+}
