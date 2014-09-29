baae47df9f71e623c7da5234f0847a8ec4d1d1eb
joao vidal <joao.vidal@exla.com.br>
2014-09-29 16:08:31 -0300

    Site Final

--- js/bebidasController.js
+++ js/bebidasController.js
@@ -1,208 +1,196 @@
+/*globals angular, $, console*/
+
 var bebidasApp = angular.module('bebidasApp', []);
 
 
 function atualizar(scope, funcao) {
-
+    'use strict';
     if (scope.$$phase || scope.$root.$$phase) {
-		if (typeof funcao !== 'function') {
-			return;
-		}    	
-		funcao();
-    }  else {
-		if (typeof funcao !== 'function') {
-	    	scope.$apply();
-    	} else {
-    		scope.$apply(funcao);
-    	}
-	}
+        if (typeof funcao !== 'function') {
+            return;
+        }
+        funcao();
+    } else {
+        if (typeof funcao !== 'function') {
+            scope.$apply();
+        } else {
+            scope.$apply(funcao);
+        }
+    }
 }
 
 function jQuerySubmit(type, url, objeto, retorno) {
-	var parametrosEnvio = {
-        beforeSend: function(xhrObj){
-            xhrObj.setRequestHeader("X-Parse-Application-Id","L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
-            xhrObj.setRequestHeader("X-Parse-REST-API-Key","TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5");                
+    'use strict';
+    var parametrosEnvio = {
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("X-Parse-Application-Id", "L7F9ql3DctcnCtoDwah0GGG8GFw8vuWJ6OWFJgNp");
+            xhrObj.setRequestHeader("X-Parse-REST-API-Key", "TLtoYkorgdYipUyLayJnWEjZUuiuhWhTimVzJLg5");
         },
         type: type,
         url: url,
         processData: false,
         contentType: "application/json; charset=UTF-8",
         dataType: "json",
-        success: function(data){ retorno(data)},
+        success: function (data) {
+            retorno(data);
+        }
         /*error: function(){ alert("Erro ao carregar PARSE")}*/
     };
 
     if (objeto) {
-    	parametrosEnvio.data = JSON.stringify(objeto);
+        parametrosEnvio.data = JSON.stringify(objeto);
     }
 
     $.ajax(parametrosEnvio);
 }
 
 bebidasApp.controller('bebidasCtrl', ['$scope', function ($scope) {
-	var bebidaInicial = {
-		nome: '',
-		descr: '',
-		preco: '',
-		teor: '',
-		origem: '',
-		estilo: '',
-		bandeira: ''
-	};
-	$scope.bebidas = [];
+    'use strict';
+    var bebidaInicial = {
+        nome: '',
+        descr: '',
+        preco: '',
+        teor: '',
+        origem: '',
+        estilo: '',
+        bandeira: ''
+    };
+    $scope.bebidas = [];
 
-	$scope.paises = ['Brasil', 'Bélgica', 'Estados Unidos', 'Holanda', 'Irlanda', 'México', 'Dinamarca', 'Alemanha'];
-	$scope.estilos = ['Large', 'Pilsen'];
-	$scope.pagina = 0;
-	$scope.bebidaExcluir = null;
-	$scope.detalheEscolhido = null;
-	$scope.bebida = bebidaInicial;
+    $scope.paises = ['Brasil', 'Bélgica', 'Estados Unidos', 'Holanda', 'Irlanda', 'México', 'Dinamarca', 'Alemanha'];
+    $scope.estilos = ['Large', 'Pilsen'];
+    $scope.pagina = 0;
+    $scope.bebidaExcluir = null;
+    $scope.detalheEscolhido = null;
+    $scope.bebida = bebidaInicial;
 
-	$scope.reload = function (numeroPag) {
-		$scope.pagina = numeroPag;
-		$scope.bebidas = $scope.todasBebidas;
-		$scope.detalheEscolhido = null;
-		$scope.bebidaEditada = null;
-		$scope.bebida = bebidaInicial;
-	}
+    $scope.reload = function (numeroPag) {
+        $scope.pagina = numeroPag;
+        $scope.bebidas = $scope.todasBebidas;
+        $scope.detalheEscolhido = null;
+        $scope.bebidaEditada = null;
+        $scope.bebida = bebidaInicial;
+    };
 
-	$scope.definirPagina = function (numeroPagina) {
-		$scope.pagina = numeroPagina;
-	};
+    $scope.definirPagina = function (numeroPagina) {
+        $scope.pagina = numeroPagina;
+    };
 
-	$scope.definirPaises = function (nomePais) {
-		var bebidas = $scope.todasBebidas,
-			numeroBebidas = bebidas.length,
-			indiceBebida,
-			bebida,
-			bebidasPais = [];
-		
-		for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
-			bebida = bebidas[indiceBebida];
-			if (bebida.origem === nomePais) {
-				bebidasPais.push(bebida);
-			}
-		}
-		$scope.bebidas = bebidasPais;
-		atualizar($scope);
-	};
+    $scope.definirPaises = function (nomePais) {
+        var bebidas = $scope.todasBebidas,
+            numeroBebidas = bebidas.length,
+            indiceBebida,
+            bebida,
+            bebidasPais = [];
 
-	$scope.definirDetalhe = function (objectId) {
-		if ($scope.detalheEscolhido === objectId) {
-			$scope.detalheEscolhido = null;
-		} else {
-			$scope.detalheEscolhido = objectId;
-		}
-	};
+        for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
+            bebida = bebidas[indiceBebida];
+            if (bebida.origem === nomePais) {
+                bebidasPais.push(bebida);
+            }
+        }
+        $scope.bebidas = bebidasPais;
+        atualizar($scope);
+    };
 
-	$scope.habilitarCadastro = function () {
-		if ($scope.bebidaEditada === -1) {
-			$scope.bebidaEditada = null;
-		} else {
-			$scope.bebidaEditada = -1;
-		}
-		$scope.bebida = bebidaInicial;
+    $scope.definirDetalhe = function (objectId) {
+        if ($scope.detalheEscolhido === objectId) {
+            $scope.detalheEscolhido = null;
+        } else {
+            $scope.detalheEscolhido = objectId;
+        }
+    };
 
-	};
+    $scope.habilitarCadastro = function () {
+        if ($scope.bebidaEditada === -1) {
+            $scope.bebidaEditada = null;
+        } else {
+            $scope.bebidaEditada = -1;
+        }
+        $scope.bebida = bebidaInicial;
 
-	$scope.editarBebida = function (objectId) {
-		var bebidas = $scope.bebidas,
-			numeroBebidas = bebidas.length,
-			indiceBebida,
-			bebida;
+    };
 
-		$scope.bebidaEditada = objectId;
-		for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
-			bebida = bebidas[indiceBebida];
-			if (bebida) {
-				if (bebida.objectId === objectId) {
-					$scope.bebida = bebida;
-					break;
-				}
-			}
-		}
-	};
+    $scope.editarBebida = function (objectId) {
+        var bebidas = $scope.bebidas,
+            numeroBebidas = bebidas.length,
+            indiceBebida,
+            bebida;
 
-	$scope.excluir = function () {
-		$scope.bebidaExcluir = $scope.bebidaEditada;
-		atualizar($scope);
-	};
+        $scope.bebidaEditada = objectId;
+        for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
+            bebida = bebidas[indiceBebida];
+            if (bebida) {
+                if (bebida.objectId === objectId) {
+                    $scope.bebida = bebida;
+                    break;
+                }
+            }
+        }
+    };
 
-	$scope.confirmarExcluir = function () {
-		var bebidas = $scope.bebidas,
-			numeroBebidas = bebidas.length,
-			indiceBebida,
-			bebida;
+    $scope.excluir = function () {
+        $scope.bebidaExcluir = $scope.bebidaEditada;
+        atualizar($scope);
+    };
 
-		jQuerySubmit("DELETE", 'https://api.parse.com/1/classes/Cervejas/'+ $scope.bebidaExcluir, null, function(data) {
-			for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
-				bebida = bebidas[indiceBebida];
-				if (bebida) {
-					if (bebida.objectId === $scope.bebidaExcluir) {
-						bebidas.splice(indiceBebida, 1);
-						break;
-					}
-				}
-			}
-			$scope.bebidaExcluir = null;
-			$scope.bebidas = bebidas;
-			$scope.bebida = bebidaInicial;
-			atualizar($scope);
-		});
-	};
+    $scope.confirmarExcluir = function () {
+        var bebidas = $scope.bebidas,
+            numeroBebidas = bebidas.length,
+            indiceBebida,
+            bebida;
 
-	$scope.cancelarExcluir = function () {
-		$scope.bebidaExcluir = null;
-		atualizar($scope);
-	};
+        jQuerySubmit("DELETE", 'https://api.parse.com/1/classes/Cervejas/' + $scope.bebidaExcluir, null, function () {
+            for (indiceBebida = 0; indiceBebida < numeroBebidas; indiceBebida++) {
+                bebida = bebidas[indiceBebida];
+                if (bebida) {
+                    if (bebida.objectId === $scope.bebidaExcluir) {
+                        bebidas.splice(indiceBebida, 1);
+                        break;
+                    }
+                }
+            }
+            $scope.bebidaExcluir = null;
+            $scope.bebidas = bebidas;
+            $scope.bebida = bebidaInicial;
+            atualizar($scope);
+        });
+    };
 
-	jQuerySubmit("GET", 'https://api.parse.com/1/classes/Cervejas', null, function (data) {
-		var bebidas = data.results;
-		$scope.todasBebidas = bebidas;
-		$scope.bebidas = bebidas;
-		atualizar($scope);
-	});
+    $scope.cancelarExcluir = function () {
+        $scope.bebidaExcluir = null;
+        atualizar($scope);
+    };
+
+    jQuerySubmit("GET", 'https://api.parse.com/1/classes/Cervejas', null, function (data) {
+        var bebidas = data.results;
+        $scope.todasBebidas = bebidas;
+        $scope.bebidas = bebidas;
+        atualizar($scope);
+    });
 
 
-	$scope.cadastrarBebida = function () {
-		var bebida = $scope.bebida;
-		jQuerySubmit("POST", 'https://api.parse.com/1/classes/Cervejas/', bebida, function(data) {
-			console.log(data);
-		});
-	}
+    $scope.cadastrarBebida = function () {
+        var bebida = $scope.bebida;
+        jQuerySubmit("POST", 'https://api.parse.com/1/classes/Cervejas/', bebida, function (data) {
+            console.log(data);
+        });
+    };
 
-	$scope.fecharcadastro = function () {
-		$scope.bebidaEditada = null;
-	}
-	/*function confirmDelet(getID2){
-		$( "#confirmar" ).dialog({
-		          modal: true,
-		          buttons: {
-		          	Sim: function(){
-		          			
+    $scope.fecharcadastro = function () {
+        $scope.bebidaEditada = null;
+    };
 
-		          			$( this ).dialog( "close" )
-		          		},
-		          	Não: function(){$(this).dialog("close")}
-		          }
-		    });
-	}*/
 }]);
 
-function maxLength(el) {    
-    if (!('maxLength' in el)) {
+function maxLength(el) {
+    'use strict';
+    if (!(el.hasOwnProperty('maxLength'))) {
         var max = el.attributes.maxLength.value;
         el.onkeypress = function () {
-            if (this.value.length >= max) return false;
+            if (this.value.length >= max) {
+                return false;
+            }
         };
     }
 }
-
-
-   	var pattern = /^(?:\s*\d{8}\s*(?:,|$))+$/;
-	var testvalue = document.getElementById('checkNumber').value;
-	if(pattern.test(testvalue)){
-	alert("Correct");
-	} else {
-	alert("Wrong");
-	}       
